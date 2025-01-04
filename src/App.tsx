8fb838cecaea95280879ae837c91650025c8b231
Richar <2958588231@qq.com>
2025-01-04 17:35:51 +0800

    build: 基建

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import React from "react";
+import "./app.less";
+// 小于10kb的图片被转成了base64位格式的
+import one from "./assets/images/222.png";
+import two from "./assets/images/111.png";
 
 function App() {
-  return <h2>webpack5-react-ts</h2>;
+  return (
+    <>
+      <h2>webpack5-react-ts</h2>;{/* <img src={one} alt="小于20kb的图片" /> */}
+      {/* <img src={two} alt="大于于20kb的图片" /> */}
+      <div className="smallImg"></div> {/* 小图片背景容器 */}
+      <div className="bigImg"></div> {/* 大图片背景容器 */}
+    </>
+  );
 }
 export default App;
--- webpack.base.js
+++ webpack.base.js
@@ -1,12 +1,12 @@
 // webpack.base.js
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-
+const webpack = require("webpack");
 module.exports = {
-  entry: path.join(__dirname, "../src/index.tsx"), // 入口文件
+  entry: path.join(__dirname, "./src/index.tsx"), // 入口文件
   output: {
     filename: "static/js/[name].js", // 每个输出js的名称
-    path: path.join(__dirname, "../dist"), // 打包结果输出路径
+    path: path.join(__dirname, "./dist"), // 打包结果输出路径
     clean: true, // webpack4需要配置clean-webpack-plugin来删除dist文件,webpack5内置了
     publicPath: "/", // 打包后文件的公共前缀路径
   },
@@ -14,12 +14,22 @@ module.exports = {
     rules: [
       {
         test: /.(ts|tsx)$/, // 匹配.ts, tsx文件
-        use: {
-          loader: "babel-loader",
-          options: {
-            // 预设执行顺序由右往左,所以先处理ts,再处理jsx
-            presets: ["@babel/preset-react", "@babel/preset-typescript"],
+        use: "babel-loader",
+      },
+      {
+        test: /.(css|less)$/, //匹配 css和less 文件
+        use: ["style-loader", "css-loader", "postcss-loader", "less-loader"],
+      },
+      {
+        test: /.(png|jpg|jpeg|gif|svg)$/, // 匹配图片文件
+        type: "asset", // type选择asset
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10 * 1024, // 小于10kb转base64位
           },
+        },
+        generator: {
+          filename: "static/images/[name][ext]", // 文件输出目录和命名
         },
       },
     ],
@@ -29,8 +39,14 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "../public/index.html"), // 模板取定义root节点的模板
+      template: path.resolve(__dirname, "./public/index.html"), // 模板取定义root节点的模板
       inject: true, // 自动注入静态资源
+    }),
+    new webpack.DefinePlugin({
+      "process.env.BASE_ENV": JSON.stringify(process.env.BASE_ENV),
     }),
   ],
 };
+
+console.log("NODE_ENV", process.env.NODE_ENV);
+console.log("BASE_ENV", process.env.BASE_ENV);
